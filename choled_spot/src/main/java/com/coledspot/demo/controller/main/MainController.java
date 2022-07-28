839bbc37742b16212266fc665dc0a7531a1610bd
Khark <kunoo913@naver.com>
2022-07-28 22:21:55 +0900

    y not working?

--- choled_spot/src/main/java/com/coledspot/demo/controller/main/MainController.java
+++ choled_spot/src/main/java/com/coledspot/demo/controller/main/MainController.java
@@ -1,9 +1,12 @@
 package com.coledspot.demo.controller.main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coledspot.demo.domain.maria.member.MemberReqDTO;
 
 @Controller
 @RequestMapping(path = "/")
@@ -19,10 +22,11 @@ public class MainController {
 		return "main/home";
 	}
 	
+	
 	@GetMapping("signup")
-	public String signup() {
+	public String signup(@ModelAttribute("MemberReqDTO") MemberReqDTO memberReqDto, ModelMap model )  {
 		
-		
+		model.addAttribute("MemberReqDTO", memberReqDto);
 		return "main/signup";
 	}
 	
--- choled_spot/src/main/java/com/coledspot/demo/controller/main/MainRestController.java
+++ choled_spot/src/main/java/com/coledspot/demo/controller/main/MainRestController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,19 +12,19 @@ import com.coledspot.demo.domain.maria.member.MemberReqDTO;
 import com.coledspot.demo.svc.member.MemberSvc;
 
 @RestController
-@RequestMapping(path = "/rest/*")
+@RequestMapping("rest/*")
 public class MainRestController {
 
 	@Autowired
 	public MemberSvc membersvc;
 	
 	@PostMapping("signup")
-	public String signup(MemberReqDTO dto, HttpServletRequest request ) throws Exception {
-		System.out.println("#############rest/signup");
+	public String signup(@RequestBody MemberReqDTO dto, HttpServletRequest request )  {
+
 		String result = "";
 		
 		
-		membersvc.save(dto);
+		// membersvc.save(dto);
 				
 		return result;
 	}
